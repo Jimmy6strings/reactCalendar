52b7fa88612433bceb02eae6d8c9d145276746d3
James Mitchell <mitchell.thehumantornado@gmail.com>
2017-08-15 15:14:22 -0500

    added component and finished bundle

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+import React from 'react';
+import {render} from 'react-dom';
+import aComponent from './aComponent.jsx';
+
+
+class App extends React.Component {
+  render () {
+    return
+        <div>
+          <p> Hello React!</p>
+          <aComponent />
+        </div>
+  }
+}
+
+render(<App/>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
@@ -9,6 +10,16 @@ var config = {
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
   }
 };
 
